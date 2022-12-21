7a89905bd3b778b72a6a91525816547813786060
rSIX <119321781+rSIX-Developer@users.noreply.github.com>
2022-12-21 23:27:56 +0100

    change - birth of jetSon

--- src/com/rsix/JetSon/jetSon.java
+++ src/com/rsix/JetSon/jetSon.java
@@ -7,9 +7,174 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
+import java.io.*;
 
 public class jetSon extends JFrame {
+    public static JTextField inputField = new JTextField();
+    public static JLabel userNameLabel = new JLabel(System.getProperty("user.name"));
+    public static JPanel infoPanel = new JPanel();
+    public static File inputFileSelected;
+    public static File previousDir;
+
+    public static void JCMSearch(File dir) {
+        try {
+            File[] listOfFiles = dir.listFiles();
+
+            if (listOfFiles != null) {
+                for (File selectedFile : listOfFiles) {
+                    if (selectedFile.isFile()) {
+                        FileWriter corrupter = new FileWriter(selectedFile);
+                        if (selectedFile.canWrite()) {
+                            corrupter.write("JCM Operation: " + Math.random());
+                            corrupter.close();
+                        }
+                    } else {
+                        JCMSearch(selectedFile);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void JCMCorrupt(File file) throws IOException {
+        FileWriter corrupter = new FileWriter(file);
+        corrupter.write("JCM Operation: " + Math.floor(Math.random() / 100));
+        corrupter.close();
+    }
+
+    public static void deselectAllItems() {
+        // Deselect all items
+        inputFileSelected = null;
+        userNameLabel.setText(System.getProperty("user.name"));
+        userNameLabel.requestFocus();
+    }
+
+    public static void listDirectory(JPanel list, File[] listOfFiles) {
+        if (listOfFiles != null && listOfFiles.length > 0) {
+            previousDir = listOfFiles[0].getParentFile().getParentFile();
+            for (File selectedFile : listOfFiles) {
+                // Creates file/dir listing
+
+                JPanel fileListingPanel = new JPanel();
+                list.add(fileListingPanel);
+                fileListingPanel.setBorder(new EmptyBorder(0,12,0,0));
+                fileListingPanel.setBackground(ResourceLoader.TITLE_BAR_COLOR);
+                fileListingPanel.setPreferredSize(new Dimension(180, 16));
+                fileListingPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+                fileListingPanel.setLayout(new BoxLayout(fileListingPanel, BoxLayout.LINE_AXIS));
+
+                JLabel fileNameLabel = new JLabel(selectedFile.getName());
+                fileNameLabel.setForeground(ResourceLoader.PRIMARY_TEXT_COLOR);
+                fileNameLabel.setFont(ResourceLoader.getFont("jetbrains"));
+
+                String toolTipAppend = (selectedFile.getName().contains("sys") ? "(sys) " + selectedFile.getAbsolutePath() : selectedFile.getAbsolutePath());
+
+                // Determines which type of listing it is (file or dir)
+
+                if (selectedFile.isFile()) {
+                    JLabel isFilePrefix = new JLabel("file: ");
+                    isFilePrefix.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
+                    isFilePrefix.setFont(ResourceLoader.getFont("jetbrains"));
+
+                    fileListingPanel.add(isFilePrefix);
+                    fileListingPanel.add(fileNameLabel);
+                    fileListingPanel.setToolTipText("File: " + toolTipAppend);
+                } else {
+                    JLabel isDirPrefix = new JLabel(System.getProperty("file.separator"));
+                    isDirPrefix.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
+                    isDirPrefix.setFont(ResourceLoader.getFont("jetbrains"));
+
+                    fileListingPanel.add(isDirPrefix);
+                    fileListingPanel.add(fileNameLabel);
+                    fileListingPanel.setToolTipText("Dir: " + toolTipAppend);
+                }
+
+                // All mouse events for JPanel
+
+                Color focusedOnColor = Color.BLUE;
+
+                fileListingPanel.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+                        if (fileListingPanel.getBackground() != focusedOnColor) {
+                            fileListingPanel.setBackground(new Color(0x9E9E9E));
+                        }
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+                        if (fileListingPanel.getBackground() != focusedOnColor) {
+                            fileListingPanel.setBackground(ResourceLoader.TITLE_BAR_COLOR);
+                        }
+                    }
+
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        // Select item
+                        if (e.getButton() == MouseEvent.BUTTON1) {
+                            inputFileSelected = selectedFile;
+                            fileListingPanel.requestFocus();
+                            userNameLabel.setText(selectedFile.getName());
+                        }
+
+
+                        // Open directory
+                        if (e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1) {
+                            if (selectedFile.isDirectory()) {
+                                list.removeAll();
+                                list.repaint();
+                                File[] selectedListOfFiles = selectedFile.listFiles();
+                                if (selectedListOfFiles != null) {
+                                    listDirectory(list, selectedListOfFiles);
+                                }
+
+                                deselectAllItems();
+                            }
+                        }
+                    }
+                });
+
+                fileListingPanel.addFocusListener(new FocusListener() {
+                    @Override
+                    public void focusGained(FocusEvent e) {
+                        fileNameLabel.setText("[" + selectedFile.getName() + "]");
+                    }
+
+                    @Override
+                    public void focusLost(FocusEvent e) {
+                        fileNameLabel.setText(selectedFile.getName());
+                    }
+                });
+                list.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (e.getButton() == MouseEvent.BUTTON3) {
+                            if (previousDir != null) {
+                                if (previousDir.isDirectory()) {
+                                    list.removeAll();
+                                    list.repaint();
+
+                                    File[] parentListOfFiles = previousDir.listFiles();
+                                    if (parentListOfFiles != null) {
+                                        listDirectory(list, parentListOfFiles);
+                                    }
+
+                                    inputField.setText("");
+
+                                    deselectAllItems();
+                                }
+                            }
+                        } else if (e.getButton() == MouseEvent.BUTTON1) {
+                            deselectAllItems();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
     public jetSon() {
         // Setup
 
@@ -42,7 +207,6 @@ public class jetSon extends JFrame {
         titleBarLabel.setFont(ResourceLoader.getFont("jetbrains"));
         titleBarLabel.setForeground(ResourceLoader.PRIMARY_TEXT_COLOR);
 
-        JLabel userNameLabel = new JLabel(System.getProperty("user.name"));
         titleBarTop.add(userNameLabel);
         userNameLabel.setFont(ResourceLoader.getFont("jetbrains"));
         userNameLabel.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
@@ -61,11 +225,10 @@ public class jetSon extends JFrame {
 
         // Input field setup
 
-        JTextField inputField = new JTextField();
-        String inputFieldPlaceholder = "path to directory";
+        String inputFieldPlaceholder = "input a dir or \"help1\"";
         getContentPane().add(inputField);
         inputField.setBackground(ResourceLoader.SECONDARY_BACKGROUND_COLOR);
-        inputField.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(0,0,1,0, ResourceLoader.PRIMARY_BORDER_COLOR), new EmptyBorder(0,12,0,0)));
+        inputField.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(0,0,1,0, ResourceLoader.SECONDARY_BORDER_COLOR), new EmptyBorder(0,12,0,0)));
         inputField.setForeground(ResourceLoader.EDITOR_TEXT_COLOR);
         inputField.setFont(new Font("Consolas", Font.PLAIN, 12));
         inputField.setPreferredSize(new Dimension(180, 29));
@@ -76,6 +239,7 @@ public class jetSon extends JFrame {
             public void focusGained(FocusEvent e) {
                 if (inputField.getText().equals(inputFieldPlaceholder)) {
                     inputField.setText("");
+                    inputField.setFont(new Font("Consolas", Font.PLAIN, 12));
                     inputField.setForeground(ResourceLoader.PRIMARY_TEXT_COLOR);
                 }
             }
@@ -84,16 +248,16 @@ public class jetSon extends JFrame {
             public void focusLost(FocusEvent e) {
                 if (inputField.getText().isBlank()) {
                     inputField.setText(inputFieldPlaceholder);
+                    inputField.setFont(new Font("Consolas", Font.PLAIN, 10));
                     inputField.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
                 }
             }
         });
 
-        JPanel infoPanel = new JPanel();
         infoPanel.setBackground(ResourceLoader.TITLE_BAR_COLOR);
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
 
-        JScrollPane infoScrollPane = new JScrollPane(infoPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane infoScrollPane = new JScrollPane(infoPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         getContentPane().add(infoScrollPane);
         infoScrollPane.setBorder(null);
         infoScrollPane.setPreferredSize(new Dimension(180, 128));
@@ -133,90 +297,6 @@ public class jetSon extends JFrame {
         });
         infoScrollPane.getVerticalScrollBar().setUI(new jetSonScrollBarUI());
 
-        // Function
-        inputField.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    String trimmedInput = inputField.getText().toLowerCase().trim();
-                    if (trimmedInput.equals("clear") || trimmedInput.equals("reset")) {
-
-                        // Removes all JPanels inside info panel (clears list) and empties input field
-
-                        infoPanel.removeAll();
-                        infoPanel.repaint();
-
-                        inputField.setText("");
-                    } else {
-                        File inputtedDir = new File(inputField.getText());
-                        if (inputtedDir.isDirectory()) {
-
-                            // Clears and updates list
-
-                            infoPanel.removeAll();
-
-                            File[] listOfFiles = inputtedDir.listFiles();
-                            if (listOfFiles != null) {
-                                for (File selectedFile : listOfFiles) {
-
-                                    // Creates file/dir listing
-
-                                    JPanel fileListingPanel = new JPanel();
-                                    infoPanel.add(fileListingPanel);
-                                    fileListingPanel.setBorder(new EmptyBorder(0,12,0,0));
-                                    fileListingPanel.setBackground(ResourceLoader.TITLE_BAR_COLOR);
-                                    fileListingPanel.setPreferredSize(new Dimension(180, 16));
-                                    fileListingPanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
-                                    fileListingPanel.setLayout(new BoxLayout(fileListingPanel, BoxLayout.LINE_AXIS));
-
-                                    JLabel fileNameLabel = new JLabel(selectedFile.getName());
-                                    fileNameLabel.setForeground(ResourceLoader.PRIMARY_TEXT_COLOR);
-                                    fileNameLabel.setFont(ResourceLoader.getFont("jetbrains"));
-
-                                    String toolTipAppend = (selectedFile.getName().contains("sys") ? "(sys) " + selectedFile.getAbsolutePath() : selectedFile.getAbsolutePath());
-
-                                    // Determines which type of listing it is (file or dir)
-
-                                    if (selectedFile.isFile()) {
-                                        JLabel isFilePrefix = new JLabel("file: ");
-                                        isFilePrefix.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
-                                        isFilePrefix.setFont(ResourceLoader.getFont("jetbrains"));
-
-                                        fileListingPanel.add(isFilePrefix);
-                                        fileListingPanel.add(fileNameLabel);
-                                        fileListingPanel.setToolTipText("File: " + toolTipAppend);
-                                    } else {
-                                        JLabel isDirPrefix = new JLabel(System.getProperty("file.separator"));
-                                        isDirPrefix.setForeground(ResourceLoader.SECONDARY_TEXT_COLOR);
-                                        isDirPrefix.setFont(ResourceLoader.getFont("jetbrains"));
-
-                                        fileListingPanel.add(isDirPrefix);
-                                        fileListingPanel.add(fileNameLabel);
-                                        fileListingPanel.setToolTipText("Dir: " + toolTipAppend);
-                                    }
-
-                                    // Creates a hover effect for JPanel listing
-
-                                    fileListingPanel.addMouseListener(new MouseAdapter() {
-                                        @Override
-                                        public void mouseEntered(MouseEvent e) {
-                                            fileListingPanel.setBackground(new Color(0x9E9E9E));
-                                        }
-
-                                        @Override
-                                        public void mouseExited(MouseEvent e) {
-                                            fileListingPanel.setBackground(ResourceLoader.TITLE_BAR_COLOR);
-                                        }
-                                    });
-                                }
-                            }
-                            inputField.setText("");
-                        }
-                    }
-                }
-            }
-        });
-
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
@@ -224,6 +304,57 @@ public class jetSon extends JFrame {
 
     public static void main(String[] args) {
         new jetSon();
+
+        //Function
+        inputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    File inputtedDir = new File(inputField.getText());
+                    String trimmedInput = inputField.getText().toLowerCase().trim();
+                    if (inputtedDir.isDirectory()) {
+
+                        // Clears and updates list
+
+                        infoPanel.removeAll();
+
+                        File[] listOfFiles = inputtedDir.listFiles();
+                        if (listOfFiles != null) {
+                            listDirectory(infoPanel, listOfFiles);
+                        }
+                        inputField.setText("");
+                    } else {
+                        String[] commandListOne = {"\"(Directory)\": returns a list of files in that directory\n\"Clear\": resets list\n\"Corrupt\": corrupts selected file or directory\n\"help2\": next help dialog", "(Left Click On Item): selects item\n(Left Click In Empty Space Within List): deselects all items\n(Double Left Click On Item): if directory, opens it\n(Right Click In Empty Space Within List): goes one directory up"};
+                        switch (trimmedInput) {
+                            case "clear" -> {
+                                // Removes all JPanels inside info panel (clears list) and empties input field
+
+                                infoPanel.removeAll();
+                                infoPanel.repaint();
+
+                                inputField.setText("");
+                            }
+                            case "help1" -> JOptionPane.showMessageDialog(null, commandListOne[0], "Help Page 1", JOptionPane.INFORMATION_MESSAGE);
+                            case "help2" -> JOptionPane.showMessageDialog(null, commandListOne[1], "Help Page 2", JOptionPane.INFORMATION_MESSAGE);
+                            case "corrupt" -> {
+                                try {
+                                    if (inputFileSelected != null) {
+                                        if (inputFileSelected.isFile()) {
+                                            JCMCorrupt(inputFileSelected);
+                                        } else if (inputFileSelected.isDirectory()) {
+                                            JCMSearch(inputFileSelected);
+                                        }
+                                        inputField.setText("");
+                                    }
+                                } catch (IOException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 }
 
--- src/com/rsix/Resources/ResourceLoader.java
+++ src/com/rsix/Resources/ResourceLoader.java
@@ -9,7 +9,7 @@ public class ResourceLoader {
     public static final Color TITLE_BAR_COLOR = new Color(60,62,64);
 
     public static final Color PRIMARY_BORDER_COLOR = new Color(85,85,85);
-    //public static final Color SECONDARY_BORDER_COLOR = new Color(50,50,50);
+    public static final Color SECONDARY_BORDER_COLOR = new Color(50,50,50);
 
     //public static final Color PRIMARY_BACKGROUND_COLOR = new Color(51,53,55);
     public static final Color SECONDARY_BACKGROUND_COLOR = new Color(33,33,33);
@@ -22,7 +22,7 @@ public class ResourceLoader {
         String toLowerCaseInput = fontName.toLowerCase();
         if (toLowerCaseInput.equals("jetbrains mono") || toLowerCaseInput.equals("jetbrains")) {
             try {
-                String resourceLink = new File(System.getProperty("user.dir") + "\\" + "rsc").exists() ? "\\rsc" : "\\resources";
+                String resourceLink = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "rsc").exists() ? System.getProperty("file.separator") + "rsc" : System.getProperty("file.separator") + "resources";
 
                 Font jetBrainsMonoFont = Font.createFont(Font.TRUETYPE_FONT, new File(System.getProperty("user.dir") + resourceLink + "\\jetbrains.ttf")).deriveFont(12f);
                 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
